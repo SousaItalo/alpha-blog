f805208daed034a59926f534fc81506e9f7602bd
SousaItalo <italops94@gmail.com>
2017-03-20 13:41:10 -0300

    List all, edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
 	def new
 		@article = Article.new
 	end
 
 	def create
 		# render plain: params[:article].inspect
-		
 		@article = Article.new(article_params)
 		
 		if @article.save
@@ -19,6 +23,20 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Artigo editado com sucesso!"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 
 	private
